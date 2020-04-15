ae197929baf48e50923f3b19a87144aa62fdb373
BERKANE <chihab.berkane@cgi.com>
2020-04-16 00:45:44 +0200

    amélioration css

--- src/pages/project/scripts/descriptor_opener.js
+++ src/pages/project/scripts/descriptor_opener.js
@@ -3,10 +3,7 @@
     /* VARIABLES */
     const opener = document.getElementById('descriptor-opener');
     const descriptorElt = document.getElementById('descriptor-div');
-    const closer = document.getElementById('le-frmeur');
-
-    console.log("HEllo, ca marche ma guele");
-    
+    const closer = document.getElementById('le-frmeur');    
     
     
     opener.addEventListener('click', () => {
